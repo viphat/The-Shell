9647b5a58f15dc525760c3d19a15dcff71302a52
Eddie Yang <viphat@gmail.com>
2015-06-26 08:54:56 +0700

    Fix, Do not show TOC If No Heading

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Forked dựa trên The Shell Theme for [Ghost](http://github.com/tryghost/ghost/
 * Add Social Icon - FontAwesome (Thêm vào Footer để link tới Profile Social)
 * Thay Highlighter Prettify.js bằng highlightjs.org (Vì highlightjs có hỗ trợ Language Auto Detect)
 * Sửa lại CSS của Table trong Bài viết
+* Tự động thêm Table Of Content (TOC) dựa vào Heading của Bài viết
 
 **Screenshot**
 
@@ -18,6 +19,6 @@ Forked dựa trên The Shell Theme for [Ghost](http://github.com/tryghost/ghost/
 **Upcoming**:
 
 * Thêm Menu Navigation giống Theme Abc
-* Table Of Content for Post Item
+* Thêm Emoticon cho Blog
 
 You can find live example [here](http://notes.viphat.work/).
--- assets/js/shell.js
+++ assets/js/shell.js
@@ -3,29 +3,35 @@ $(function() {
     hljs.highlightBlock(block);
   });
 
-  $('#toc').toc({
-    container: 'section', //element to find all selectors in
-    selectors: 'h4,h5,h6', //elements to use as headings
-    smoothScroller: true, //enable or disable smooth scrolling on click
-    prefix: 'toc', //prefix for anchor tags and class names
-    onHighlight: function(el) {
-      //called when a new section is highlighted
-    },
+  if ($('section h4').length > 0) {
+    $('#toc').toc({
+      container: 'section', //element to find all selectors in
+      selectors: 'h4,h5,h6', //elements to use as headings
+      smoothScroller: true, //enable or disable smooth scrolling on click
+      prefix: 'toc', //prefix for anchor tags and class names
+      onHighlight: function(el) {
+        //called when a new section is highlighted
+      },
 
-    highlightOnScroll: true, //add class to heading that is currently in focus
-    highlightOffset: 100, //offset to trigger the next headline
-    anchorName: function(i, heading, prefix) { //custom function for anchor name
-      return prefix + i;
-    },
+      highlightOnScroll: true, //add class to heading that is currently in focus
+      highlightOffset: 100, //offset to trigger the next headline
+      anchorName: function(i, heading, prefix) { //custom function for anchor name
+        return prefix + i;
+      },
 
-    headerText: function(i, heading, $heading) { //custom function building the header-item text
-      text = $heading.text();
-      shortText = jQuery.trim(text).substring(0,25).split(" ").join(" ") + "...";
-      return shortText;
-    },
+      headerText: function(i, heading, $heading) { //custom function building the header-item text
+        text = $heading.text();
+        shortText = jQuery.trim(text).substring(0,25).split(" ").join(" ") + "...";
+        return shortText;
+      },
 
-    itemClass: function(i, heading, $heading, prefix) { // custom function for item class
-      return $heading[0].tagName.toLowerCase();
-    }
-  });
+      itemClass: function(i, heading, $heading, prefix) { // custom function for item class
+        return $heading[0].tagName.toLowerCase();
+      }
+    });
+  } else {
+    $('#toc').css('display','none');
+  }
+
+
 });
